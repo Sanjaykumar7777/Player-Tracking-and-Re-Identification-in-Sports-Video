126dd7a5e58c0a7a1ad0bde901b271253a5d590d
SanjayKumarReddi <141602579+Sanjaykumar7777@users.noreply.github.com>
2025-06-30 16:28:17 +0530

    Initial Commit

---
+++ utils.py
@@ -0,0 +1,9 @@
+import cv2
+
+def initialize_video_io(input_path, output_path):
+    cap = cv2.VideoCapture(input_path)
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    width, height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+    return cap, out, fps, width, height
